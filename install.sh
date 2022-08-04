4c0cc93338cc853b03fa4c880df4617f482060fd
Oreomeow <62660101+Oreomeow@users.noreply.github.com>
2022-08-04 19:14:56 +0800

    :saprkles: Update install.sh
    auto clean

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 pip3 install -r requirements.txt
+systemctl stop warpplus
+rm -rf /var/log/journal/*
 chmod 755 warpplus.service
 cp -f warpplus.service /etc/systemd/system/warpplus.service
 systemctl daemon-reload
